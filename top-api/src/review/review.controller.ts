923608c479ac4856f7f4ad1c3328a1a1f89abe5b
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-10-14 17:49:42 +0300

    w14.10.2022

--- top-api/src/review/review.controller.ts
+++ top-api/src/review/review.controller.ts
@@ -1,21 +1,27 @@
-import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpException, HttpStatus, Param, Post } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
+import { REVIEW_NOT_FOUND } from './review.constants';
+import { ReviewService } from './review.service';
 
 @Controller('review')
 export class ReviewController {
+  constructor(private readonly reviewServise: ReviewService) { }
 
   @Post('create')
   async create(@Body() dto: CreateReviewDto) {
-
+    return this.reviewServise.create(dto)
   }
 
   @Delete(':id')
   async delete(@Param('id') id: string) {
-
+    const deletedDoc = await this.reviewServise.delete(id)
+    if (!deletedDoc) {
+      throw new HttpException(REVIEW_NOT_FOUND, HttpStatus.NOT_FOUND)
+    }
   }
   
   @Get('byProduct/:productId')
   async getByProduct(@Param('productId') productId: string) {
-
+    return this.reviewServise.findByProductId(productId)
   }
 }
