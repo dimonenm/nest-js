b10c5ebee9c4a03caba64d124ac1fa8aab686175
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-10-26 17:15:34 +0300

    w26.10.2022

---
+++ top-api/src/auth/auth.constants.ts
@@ -0,0 +1,1 @@
+export const ALREADY_REGISTEREED_ERROR = 'Такой пользователь уже был зарегистрирован'
--- top-api/src/auth/auth.controller.ts
+++ top-api/src/auth/auth.controller.ts
@@ -1,13 +1,20 @@
-import { Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { BadRequestException, Body, Controller, HttpCode, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import { ALREADY_REGISTEREED_ERROR } from './auth.constants';
+import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService){}
 
   @UsePipes(new ValidationPipe())
   @Post('register')
   async register(@Body() dto: AuthDto) {
-
+    const oldUser = await this.authService.findUser(dto.login)
+    if (oldUser) {
+      throw new BadRequestException(ALREADY_REGISTEREED_ERROR)
+    }
+    return this.authService.createUser(dto)
   }
 
   @HttpCode(200)
