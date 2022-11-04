acada164d6b03339b45a6c00eef5f272583540bf
arzuakov@yahoo.com <arzuakov@yahoo.com>
2022-11-04 14:36:48 +0300

    w04.11.2022

--- top-api/src/product/product.service.ts
+++ top-api/src/product/product.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { ModelType } from '@typegoose/typegoose/lib/types';
 import { InjectModel } from 'nestjs-typegoose';
+import { ReviewModel } from 'src/review/review.model';
 import { CreateProductDto } from './dto/create-product.dto';
+import { FindProductDto } from './dto/find-product.dto';
 import { ProductModel } from './product.model';
 
 @Injectable()
 export class ProductService {
   constructor(@InjectModel(ProductModel) private readonly productModel: ModelType<ProductModel>) { }
-  
+
   async create(dto: CreateProductDto) {
     return this.productModel.create(dto)
   }
@@ -21,6 +23,39 @@ export class ProductService {
   }
 
   async updateById(id: string, dto: CreateProductDto) {
-    return this.productModel.findByIdAndUpdate(id, dto, {new: true}).exec()
+    return this.productModel.findByIdAndUpdate(id, dto, { new: true }).exec()
+  }
+
+  async findWithReviews(dto: FindProductDto) {
+    return this.productModel.aggregate(
+      [
+        {
+          $match: {
+            categories: dto.category
+          }
+        },
+        {
+          $sort: { _id: 1 }
+        },
+        { $limit: dto.limit },
+        {
+          $lookup: {
+            from: 'Review',
+            localField: '_id',
+            foreignField: 'productId',
+            as: 'review'
+          }
+        },
+        {
+          $addFields: {
+            reviewCount: { size: '$review' },
+            reviewAvg: {
+              $avg: '$review.rating'
+            }
+          }
+        }
+      ]
+    ).exec()
+    // ).exec() as (ProductModel & { review: ReviewModel[], reviewCount: number, reviewAvg: number })[];
   }
 }
